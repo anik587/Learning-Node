3041ac8b7a8801bbd7f28b2283c3c8b2d2149b6d
anik587 <anik587@gmail.com>
2019-08-25 11:07:42 +0600

    npm init creates package json and validator added

--- nodes-app/file.js
+++ nodes-app/file.js
@@ -4,7 +4,7 @@ fs.appendFileSync('node2.txt', 'this is the text i would love to add22s\n', (err
     if (err) throw err;
     console.log('append successfully');
 });
- fs.unlink('node.txt', (err)=>{
+ fs.unlink('node2.txt', (err)=>{
      if(err) console.log(err);
      console.log('deleted successfully');
  });
