5b0f6562177bfd385e73cffa71532b2b45cdf891
anik587 <anik587@gmail.com>
2019-09-26 18:48:43 +0600

    async await

--- nodes-app/async-await.js
+++ nodes-app/async-await.js
@@ -1,7 +1,17 @@
-const doWork = () => {
-    return new Promise((resolve, reject) => {
-        return resolve('anik')
+const add = (a, b)=>{
+    return new Promise(( resolve, reject)=>{
+        setTimeout(()=>{
+            if(a < 0 || b < 0)
+            reject('Only positive number required')
+            resolve(a+b)
+        }, 2000)
     })
+}
+
+const doWork = async () => {
+    const sum = await add(1, 99)
+    const sum2 = await add(sum, 20)
+    return await add(sum, -sum2)
 }
 
 doWork().then( result => {
